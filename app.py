fadbf8be9487bb5f74b078eb45d42555761f78a9
Vaishnavi vikas chavan <vaishnavichavan2612@gmail.com>
2026-04-26 15:36:22 +0530

    Update home route message for CI/CD pipeline

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from CI/CD Pipeline 🚀"
-
+    return "CI/CD is working 🚀"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
